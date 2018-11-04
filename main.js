fdb2bdb2036790aae1351bb173b080f362c2595b
Arnold <arnold.zephir@gmail.com>
2018-11-04 19:31:54 +0100

    FF: fix module

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 'use strict';
 import Unit from './unit.js';
 
+
+
+let a = new Unit("ball");
+console.log(a.name);
+console.log(a.hp);
+a.hurt(44);
+console.log(a.hp);
+a.hurt(44);
+console.log(a.hp);
+
--- unit.js
+++ unit.js
@@ -4,16 +4,15 @@
  * Unit Class
  */
 'use strict';
-export let name1, name2;
 
- export default class Unit {
+export default class Unit {
 
   constructor(name="anonymous") {
     this.name=name;
     this.HP=100;
   };
 
- 
+
   get hp() {
     return this.HP;
   }
@@ -22,12 +21,3 @@ export let name1, name2;
     this.HP >= damage ? this.HP -= damage : this.HP=0;
   }
 }
-
-let a = new Unit("ball");
-console.log(a.name);
-console.log(a.hp);
-a.hurt(78);
-console.log(a.hp);
-a.hurt(78);
-console.log(a.hp);
-
