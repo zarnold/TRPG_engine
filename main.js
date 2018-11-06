02a06badee661530471899c1fd381e4b563e3ffd
Arnold <arnold.zephir@gmail.com>
2018-11-06 23:00:34 +0100

    NF: add a moveTo function to unit

--- main.js
+++ main.js
@@ -2,5 +2,35 @@
 import Unit from './unit.js';
 import Stage from './stage.js';
 
-let stage = new Stage('screen');
+let stage = new Stage('screen', 100,100);
 let stage_b = new Stage('screen_B');
+
+
+//adding a unit to stage override its move function
+let achille = new Unit('Achille');
+stage.addUnit(achille, 4,3);
+
+//test move
+let i=8, j=2;
+setInterval( function() {
+  achille.moveTo(i++, j++);
+}, 400);
+
+let hercule= new Unit('Hercule');
+stage_b.addUnit(hercule);
+
+//test move
+let m=4, n=2;
+setInterval( function() {
+  hercule.moveTo(m++, n++);
+}, 700);
+
+
+// perf test
+for (var k=0; k<200; k++) {
+  let h=new Unit();
+  stage.addUnit(h, Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
+        setInterval( function() {
+          h.moveTo( Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
+        }, 200);
+}
--- unit.js
+++ unit.js
@@ -10,6 +10,9 @@ export default class Unit {
   constructor(name="anonymous") {
     this.name=name;
     this.HP=100;
+
+    this.element = document.createElement("div");
+    this.element.classList.add("unit");
   };
 
 
@@ -19,5 +22,12 @@ export default class Unit {
 
   hurt(damage) {
     this.HP >= damage ? this.HP -= damage : this.HP=0;
-  }
+  };
+
+  moveTo(a,b) {
+    // Dummy function for starting
+    // Must be overridden by parent
+    console.log(`Move to ${a}, ${b}`) ;
+  };
+
 }
